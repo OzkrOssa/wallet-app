a5221b86f0e966353a6778b869341fe2f830bcc8
Oscar Ossa <oskrlex17@gmail.com>
2023-02-28 08:27:13 -0500

    add custom colors to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,14 @@ module.exports = {
     './src/components/**/*.{js,jsx,ts,tsx}'
   ],
   theme: {
-    extend: {}
+    extend: {
+      colors: {
+        primary: '#2c3659',
+        secondary: '#89cbdb',
+        tertiary: '#ef845d',
+        quaternary: '#fbca6d'
+      }
+    }
   },
   plugins: []
 }
