6af51e033fd2960b77cd8adddf651bf362ad771c
Oscar Ossa <oskrlex17@gmail.com>
2023-02-28 09:07:40 -0500

    add welcome screen to App

--- App.tsx
+++ App.tsx
@@ -1,11 +1,12 @@
 import { StatusBar } from "expo-status-bar";
 import { Text, View } from "react-native";
+import Welcome from "./src/screens/WelcomeScreen";
 
 
 export default function App() {
   return (
-    <View>
-      <Text>Wallet App</Text>
+    <View className="flex-1">
+      <Welcome/>
       <StatusBar style="auto" />
     </View>
   );
